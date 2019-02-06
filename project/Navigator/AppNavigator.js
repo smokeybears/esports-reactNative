9eb5cb3a90b512660a3396e859805a25ea571ea4
BlakeH555 <blakehill836@gmail.com>
2019-02-06 11:47:23 -0800

    Initial Commit Settings

--- project/Navigator/AppNavigator.js
+++ project/Navigator/AppNavigator.js
@@ -3,10 +3,12 @@ import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 import SignInScreen from '../Screens/SignInScreen';
 import profileScreen from '../Screens/ProfileScreen'
 import MainTabNavigator from './MainNavigator';
+import SettingsScreen from '../Screens/SettingsScreen';
 
 export default createAppContainer(createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
-  Main: profileScreen,
-  SignIn: SignInScreen,
+  Main: SettingsScreen,
+  //Main: profileScreen,
+  //SignIn: SignInScreen,
 }));
---
+++ project/Screens/SettingsScreen.js
@@ -0,0 +1,110 @@
+'use strict';
+import React, { Component } from "react";
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  Alert
+} from 'react-native';
+import SettingsList from 'react-native-settings-list';
+
+ class SettingsScreen extends Component {
+
+   constructor(props) {
+     super(props);
+     this.onValueChange = this.onValueChange.bind(this);
+     this.state = {switchValue: false};
+   }
+
+   render() {
+     return (
+       <View style={{backgroundColor:'#EFEFF4',flex:1}}>
+         <View style={{borderBottomWidth:1, backgroundColor:'#f7f7f8',borderColor:'#c8c7cc'}}>
+           <Text style={{alignSelf:'center',marginTop:30,marginBottom:10,fontWeight:'bold',fontSize:16}}>Settings</Text>
+         </View>
+         
+         <View style={{backgroundColor:'#EFEFF4',flex:1}}>
+           <SettingsList borderColor='#c8c7cc' defaultItemSize={40}>
+           <SettingsList.Header headerText='General' headerStyle={{color:'black'}}/>
+             <SettingsList.Item
+               icon={<Image style={styles.imageStyle} source={require('./about.png')}/>}
+               hasSwitch={false}
+               //switchState={this.state.switchValue}
+               //switchOnValueChange={this.onValueChange}
+               hasNavArrow={true}
+               title='Notifications'
+             />
+             <SettingsList.Item
+               icon={<Image style={styles.imageStyle} source={require('./about.png')}/>}
+               title='Night Mode'
+               hasSwitch={true}
+               switchState={this.state.switchValue}
+               switchOnValueChange={this.onValueChange}
+               hasNavArrow={false}
+             />
+             <SettingsList.Item
+               icon={<Image style={styles.imageStyle} source={require('./about.png')}/>}
+               hasSwitch={false}
+               //switchState={this.state.switchValue}
+               //switchOnValueChange={this.onValueChange}
+               hasNavArrow={true}
+               title='Payments'
+             />
+             <SettingsList.Item
+               icon={<Image style={styles.imageStyle} source={require('./about.png')}/>}
+               hasSwitch={false}
+               //switchState={this.state.switchValue}
+               //switchOnValueChange={this.onValueChange}
+               hasNavArrow={true}
+               title='Ads'
+             />
+             <SettingsList.Item
+               icon={<Image style={styles.imageStyle} source={require('./about.png')}/>}
+               hasSwitch={false}
+               //switchState={this.state.switchValue}
+               //switchOnValueChange={this.onValueChange}
+               hasNavArrow={true}
+               title='Account'
+             />
+             <SettingsList.Item
+               icon={<Image style={styles.imageStyle} source={require('./about.png')}/>}
+               hasSwitch={false}
+               //switchState={this.state.switchValue}
+               //switchOnValueChange={this.onValueChange}
+               hasNavArrow={true}
+               title='Help'
+             />
+             <SettingsList.Item
+               icon={<Image style={styles.imageStyle} source={require('./about.png')}/>}
+               hasSwitch={false}
+               hasNavArrow={true}
+               title='About'
+             />
+             
+           </SettingsList>
+         </View>
+       </View>
+     );
+   }
+
+   onValueChange(value){
+     this.setState({switchValue: value});
+   }
+ }
+
+const styles = StyleSheet.create({
+  imageStyle:{
+    marginLeft:15,
+    alignSelf:'center',
+    height:30,
+    width:30
+  },
+  titleInfoStyle:{
+    fontSize:16,
+    color: '#8e8e93'
+  }
+});
+
+module.exports = SettingsScreen;
