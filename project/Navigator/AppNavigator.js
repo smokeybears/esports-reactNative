2f39e1f84b31b6abcc465d85f806b472b7043793
BlakeH555 <blakehill836@gmail.com>
2019-02-11 11:39:15 -0800

    Routed to Main Tab Navigator
    Demo ready almost

--- project/Navigator/AppNavigator.js
+++ project/Navigator/AppNavigator.js
@@ -13,6 +13,7 @@ import HelpScreen from '../Screens/HelpScreen';
 export default createAppContainer(createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
+  Main: MainTabNavigator,
   Settings: SettingsScreen,
   Profile: profileScreen,
   SignIn: SignInScreen,
